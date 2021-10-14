20369959ff4402cf4dce56510da682b30b9c6569
Narendra Singh Mehta ziniostech <narendrasingh.mehta@ziniosedge.com>
2021-10-14 16:18:48 +0530

    : Mapped multi ClientApp

--- CoreLibrary/Startup.cs
+++ CoreLibrary/Startup.cs
@@ -4,14 +4,24 @@ using CoreLibrary.Base.Interfaces;
 using CoreLibrary.Data;
 using CoreLibrary.Services;
 using CoreLibrary.Services.Social;
+using CoreLibrary.Utility.Handlers.ExceptionHandlers;
+using CoreLibrary.Utility.Models;
 using CoreLibrary.Utility.Services;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.SpaServices.AngularCli;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Serialization;
+using System;
+using System.IO;
+using System.Net;
 using System.Text.Json.Serialization;
 
 namespace CoreLibrary
@@ -43,7 +53,7 @@ namespace CoreLibrary
             services.AddMemoryCache();
 
             services.AddHttpContextAccessor();
-            //FrontEndConfigureServices(services);
+            FrontEndConfigureServices(services);
         }
 
         public void FrontEndConfigureServices(IServiceCollection services)
@@ -64,7 +74,7 @@ namespace CoreLibrary
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             app.Configure(env);
-            //FrontEndConfigure(app, env);
+            FrontEndConfigure(app, env);
         }
 
         public void FrontEndConfigure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -75,7 +85,37 @@ namespace CoreLibrary
             }
             else
             {
-                app.UseExceptionHandler("/Error");
+                //app.UseExceptionHandler("/Error");
+
+                app.UseExceptionHandler(appError =>
+                {
+                    appError.Run(async context =>
+                    {
+                        context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                        context.Response.ContentType = "application/json";
+                        var exceptionHandler = context.Features.Get<IExceptionHandlerFeature>();
+                        var apiResponse = new APIResponse();
+                        if (exceptionHandler.Error != null)
+                        {
+                            // setting display errors
+                            if (exceptionHandler.Error is HttpResponseException ex)
+                            {
+                                apiResponse.SetDisplayError(ex.Message);
+                                apiResponse.StatusCode = ex.StatusCode;
+                            }
+                            else
+                                apiResponse.SetException(exceptionHandler.Error);
+
+                            //SentrySdk.CaptureException(exceptionHandler.Error);
+                            context.Response.StatusCode = (int)apiResponse.StatusCode;
+                        }
+
+
+                        await context.Response.WriteAsync(JsonConvert.SerializeObject(apiResponse, new JsonSerializerSettings() { ContractResolver = new CamelCasePropertyNamesContractResolver() }));
+
+                    });
+                });
+
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
@@ -102,6 +142,7 @@ namespace CoreLibrary
                     pattern: "{controller}/{action=Index}/{id?}");
             });
 
+            
             app.UseSpa(spa =>
             {
                 // To learn more about options for serving an Angular SPA from ASP.NET Core,
@@ -114,6 +155,31 @@ namespace CoreLibrary
                     spa.UseAngularCliServer(npmScript: "start");
                 }
             });
+
+            // Mapping another client app with core as a base url
+            app.Map(new PathString("/core"), client => {
+                if (env.IsDevelopment())
+                {
+                    StaticFileOptions coreLib = new StaticFileOptions()
+                    {
+                        FileProvider = new PhysicalFileProvider(
+                                Path.Combine(
+                                    Directory.GetCurrentDirectory(),
+                                    @"core-library"
+                                )
+                            )
+                    };
+                    client.UseSpaStaticFiles(coreLib);
+                    client.UseSpa(spa =>
+                    {
+                        spa.Options.StartupTimeout = new TimeSpan(0, 5, 0);
+                        spa.Options.SourcePath = "core-library";
+                        // it will use package.json & will search for start command to run
+                        spa.UseAngularCliServer(npmScript: "start");
+                    });
+                }
+            });
+
         }
     }
 }
